28f516bfc4d2c5a5799c846ad2a8037aed432d4e
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-11-18 10:57:07 -0500

    Added listing variables

--- app/src/main/java/com/detroitlabs/mentorapp/Model/ListingModel.java
+++ app/src/main/java/com/detroitlabs/mentorapp/Model/ListingModel.java
@@ -4,4 +4,9 @@ package com.detroitlabs.mentorapp.Model;
  * Created by aditishetty on 11/18/14.
  */
 public class ListingModel {
+    private String title;
+    private String author;
+    private String selfText;
+    private String ListingUrl;
+    private boolean isSelfText;
 }
