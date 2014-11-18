12bc145071b96dffa91f5264417581558909cdb9
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-11-18 15:37:12 -0500

    Added detail view fragment

---
+++ app/src/main/java/com/detroitlabs/mentorapp/fragments/DetailViewFragment.java
@@ -0,0 +1,9 @@
+package com.detroitlabs.mentorapp.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by aditishetty on 11/18/14.
+ */
+public class DetailViewFragment extends Fragment {
+}
