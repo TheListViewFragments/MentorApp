b444dad1b72a3a8c8d2e9a771675ceb7ac907c15
Kate Catlin <kate.catlin@detroitlabs.com>
2014-11-18 11:43:38 -0500

    Adding edittext and button inflation

---
+++ app/src/main/java/com/detroitlabs/mentorapp/activities/FragmentManagementActivity.java
@@ -0,0 +1,9 @@
+package com.detroitlabs.mentorapp.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by katecatlin on 11/18/14.
+ */
+public class FragmentManagementActivity extends Activity {
+}
--- app/src/main/java/com/detroitlabs/mentorapp/activities/MainActivity.java
+++ app/src/main/java/com/detroitlabs/mentorapp/activities/MainActivity.java
@@ -1,17 +1,30 @@
-package com.detroitlabs.mentorapp;
+package com.detroitlabs.mentorapp.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.detroitlabs.mentorapp.R;
 
 
 public class MainActivity extends Activity {
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.search_string_text);
+        button = (Button) findViewById(R.id.first_page_button);
+
     }
 
 
