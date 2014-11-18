f3389dbaead70fd73ccd9ce7ecc3f57646e8ec82
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-11-18 15:51:41 -0500

    Inflated detail layout for detail fragment

--- app/src/main/java/com/detroitlabs/mentorapp/fragments/DetailViewFragment.java
+++ app/src/main/java/com/detroitlabs/mentorapp/fragments/DetailViewFragment.java
@@ -1,9 +1,21 @@
 package com.detroitlabs.mentorapp.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.detroitlabs.mentorapp.R;
 
 /**
  * Created by aditishetty on 11/18/14.
  */
 public class DetailViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile_view, container, false);
+        return view;
+    }
 }
