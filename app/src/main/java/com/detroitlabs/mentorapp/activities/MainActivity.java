f69074eeab1e19b6df389f496ccb38d28c5ba3a6
Cory Gorham <cory.gorham@detroitlabs.com>
2014-11-19 10:33:13 -0500

    finishing arrayadapter for our listview

--- app/src/main/java/com/detroitlabs/mentorapp/activities/MainActivity.java
+++ app/src/main/java/com/detroitlabs/mentorapp/activities/MainActivity.java
@@ -3,11 +3,6 @@ package com.detroitlabs.mentorapp.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-<<<<<<< HEAD
-=======
-import android.view.Menu;
-import android.view.MenuItem;
->>>>>>> 5485b41df21dc4d868dd97049116322b677a1895
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,20 +13,14 @@ import com.detroitlabs.mentorapp.R;
 public class MainActivity extends Activity {
     EditText editText;
     Button button;
-<<<<<<< HEAD
-=======
     String subreddit = "";
-
     static final String SUBREDDIT_CHOICE_KEY = "SUBREDDIT_CHOICE_KEY";
->>>>>>> 5485b41df21dc4d868dd97049116322b677a1895
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
-
         editText = (EditText) findViewById(R.id.search_string_text);
         button = (Button) findViewById(R.id.first_page_button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -48,19 +37,6 @@ public class MainActivity extends Activity {
 
 
                 }
-
-            }
-        });
-    }
->>>>>>> 5485b41df21dc4d868dd97049116322b677a1895
-
-        editText = (EditText) findViewById(R.id.search_string_text);
-        button = (Button) findViewById(R.id.first_page_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent launchListViewIntent = new Intent(getApplicationContext(), SubredditListViewActivity.class);
-                startActivity(launchListViewIntent);
 
             }
         });
