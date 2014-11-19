608c6dacf8db4efe13b4580144adecdf2550e4d8
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-11-19 14:14:05 -0500

    Fixed some minor stuff

--- app/src/main/java/com/detroitlabs/mentorapp/requests/SubredditApiRequest.java
+++ app/src/main/java/com/detroitlabs/mentorapp/requests/SubredditApiRequest.java
@@ -18,16 +18,13 @@ import java.util.ArrayList;
  * Created by elyseturner on 11/19/14.
  */
 public class SubredditApiRequest extends AsyncTask<String, Void, ArrayList<ListingModel>> {
-//    private final String AUTHOR =
-//    private final String TITLE =;
-//    private final String IS_SELF_TEXT =
+
     private final String URL_BASE = "http://api.reddit.com/r/";
     private String SUBREDDIT_NAME = "";
     public String redditJsonString;
 
-
     @Override
-    protected ArrayList doInBackground(String... params) {
+    protected ArrayList<ListingModel> doInBackground(String... params) {
         HttpURLConnection urlConnection = null;
         BufferedReader bufferedReader = null;
         SUBREDDIT_NAME = params[0];
@@ -66,8 +63,7 @@ public class SubredditApiRequest extends AsyncTask<String, Void, ArrayList<Listi
             redditJsonString = buffer.toString();
 
         }catch (IOException e) {
-        // If the code didn't successfully get the weather data, there's no point in attemping
-        // to parse it.
+
         return null;
     } finally {
         if (urlConnection != null) {
@@ -92,5 +88,6 @@ public class SubredditApiRequest extends AsyncTask<String, Void, ArrayList<Listi
     @Override
     protected void onPostExecute(ArrayList<ListingModel> listingModels) {
         super.onPostExecute(listingModels);
+        // implement interface to send arraylist back to main activity
     }
 }
