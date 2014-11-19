571d7eb4802ed387fd79f12c32da46f980b796cb
elyselturner <elyse.turner@detroitlabs.com>
2014-11-19 11:45:38 -0500

    we added the JSON call

--- app/src/main/java/com/detroitlabs/mentorapp/requests/SubredditApiRequest.java
+++ app/src/main/java/com/detroitlabs/mentorapp/requests/SubredditApiRequest.java
@@ -2,28 +2,95 @@ package com.detroitlabs.mentorapp.requests;
 
 import android.os.AsyncTask;
 
-import org.apache.http.protocol.HTTP;
+import com.detroitlabs.mentorapp.model.ListingModel;
+
+import org.json.JSONException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by elyseturner on 11/19/14.
  */
-public class SubredditApiRequest extends AsyncTask<String, Void, String> {
-    private final String AUTHOR =
-    private final String TITLE =
-    private final String API_KEY =
+public class SubredditApiRequest extends AsyncTask<String, Void, ArrayList<ListingModel>> {
+//    private final String AUTHOR =
+//    private final String TITLE =;
+//    private final String IS_SELF_TEXT =
+    private final String URL_BASE = "http://api.reddit.com/r/";
+    private String SUBREDDIT_NAME = "";
+    public String redditJsonString;
+
 
     @Override
-    protected String doInBackground(String... params) {
+    protected ArrayList doInBackground(String... params) {
         HttpURLConnection urlConnection = null;
-        BufferedReader
+        BufferedReader bufferedReader = null;
+        SUBREDDIT_NAME = params[0];
 
+        try{
+            String urlString = URL_BASE + SUBREDDIT_NAME;
+
+            URL subredditURL = new URL(urlString);
+
+            urlConnection = (HttpURLConnection) subredditURL.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            redditJsonString = buffer.toString();
+
+        }catch (IOException e) {
+        // If the code didn't successfully get the weather data, there's no point in attemping
+        // to parse it.
         return null;
+    } finally {
+        if (urlConnection != null) {
+            urlConnection.disconnect();
+        }
+        if (bufferedReader != null) {
+            try {
+                bufferedReader.close();
+            } catch (final IOException e) {
+            }
+        }
+    }
+        try {
+            ArrayList<ListingModel> listingInformation = parsePostingFromJsonString(redditJsonString);
+            return listingInformation;
+
+        } catch (JSONException e) {
+            return null;
+        }
     }
 
-
-
-
+    @Override
+    protected void onPostExecute(ArrayList<ListingModel> listingModels) {
+        super.onPostExecute(listingModels);
+    }
 }
