161145e9b0a59e7c2bcc63ffea2f936d96722824
elyselturner <elyse.turner@detroitlabs.com>
2014-11-19 10:56:35 -0500

    I am starting the JSON stuff

---
+++ app/src/main/java/com/detroitlabs/mentorapp/requests/SubredditApiRequest.java
@@ -0,0 +1,29 @@
+package com.detroitlabs.mentorapp.requests;
+
+import android.os.AsyncTask;
+
+import org.apache.http.protocol.HTTP;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+
+/**
+ * Created by elyseturner on 11/19/14.
+ */
+public class SubredditApiRequest extends AsyncTask<String, Void, String> {
+    private final String AUTHOR =
+    private final String TITLE =
+    private final String API_KEY =
+
+    @Override
+    protected String doInBackground(String... params) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader
+
+        return null;
+    }
+
+
+
+
+}
